2ff33b9fcb86070cd59101a9df0f8d7e710b4809
kscript <1258@live.cn>
2019-05-28 15:51:15 +0800

    重构

--- index.js
+++ index.js
@@ -1,165 +1,154 @@
 const fs = require("fs")
 const path = require("path")
-
-function search(filePath, func, complete, option) {
-  fs.readdir(filePath, function (err, filelists) {
-    if (err) {
-      throw err
-    }
-    if (filelists && filelists.length) {
-      let count = filelists.length
-      filelists.forEach(function (filename) {
-        machining(filePath, filename, func, function (type) {
-          count--
-          count === 0 && complete && complete()
-        }, option)
-      })
-    } else {
-      complete && complete()
-    }
-  })
-}
-
-function Async(func){
-  return new Promise(function (resolve, reject) {
-    func(function(err, data){
-      if(err){
-        reject(err)
-      } else {
-        resolve(data)
-      }
+let option = null
+const cb2promise = (callback) => {
+  return new Promise((resolve, reject) => {
+    callback((err, data) => {
+      err ? reject(err) : resolve(data)
     })
   })
 }
-
-/**
- * 对文件和文件夹进行处理
- * @param {string} filePath 目录
- * @param {string} filename 文件名
- * @param {function} func 处理函数
- * @param {function} done 处理完毕回调
- */
-function machining(filePath, filename, func, done, option) {
-  if (!/^(\/|\\)$/.test(filename)) {
-    let current = path.join(filePath, filename)
-    Async(function(cb){
-      fs.open(current, 'r', cb)
-    }).then(function(file) {
-      return Async(function(cb){
-        fs.fstat(file, cb)
-      })
-    }).then(function(stats){
-      if (stats.isFile()) {
-        // 文件需要等处理完的回调
-        func({
-          isDir: false,
-          path: current,
-          name: filename
-        }, function () {
-          done('file')
+class Loader {
+  constructor(option) {
+    this.option = option
+  }
+  search(filePath) {
+    return cb2promise((cb) => {
+      fs.readdir(filePath, cb)
+    }).then(filelists => {
+      return Promise.all(
+        filelists.map(filename => {
+          return this.machining(filePath, filename)
         })
-      } else if (stats.isDirectory()) {
-        func({
-          isDir: true,
-          path: current,
-          name: filename
-        }, function (verifyed) {
-          if (verifyed || !option.deep) {
-            // 文件夹的话, 需要等search的complete回调 冒泡
-            search(current, func, function () {
-              done('dir')
-            }, option)
+      )
+    })
+  }
+  getStats(filePath) {
+    return cb2promise(cb => {
+      // 打开文件/文件夹
+      fs.open(filePath, 'r', cb)
+    })
+      .then(file => {
+        // 读取文件/文件夹信息
+        return cb2promise(cb => {
+          fs.fstat(file, cb)
+        })
+      })
+  }
+  /**
+   * 对文件和文件夹进行处理
+   * @param {string} filePath 目录
+   * @param {string} filename 文件名
+   */
+  machining(filePath, filename) {
+    const option = this.option
+    return new Promise((resolve, reject) => {
+      if (!/^(\/|\\)$/.test(filename)) {
+        let current = path.join(filePath, filename)
+        this.getStats(current)
+          .then(stats => {
+            if (stats.isFile()) {
+              if (this.verifyExt(current, option.ext)) {
+                // 文件需要等处理完的回调
+                return this.loaderHandler({
+                  isDir: false,
+                  path: current,
+                  name: filename
+                }, () => {
+                  resolve('file')
+                })
+              }
+            } else if (stats.isDirectory()) {
+              if (this.verifyDir(option.include, option.exclude, current)) {
+                return this.loaderHandler({
+                  isDir: true,
+                  path: current,
+                  name: filename
+                }, () => {
+                  if (option.deep) {
+                    // 遍历子文件夹
+                    this.search(current).then(() => {
+                      resolve('dir')
+                    })
+                  } else {
+                    resolve('dir')
+                  }
+                })
+              }
+            }
+            resolve('exclude')
+          })
+          .catch(e => {
+            reject(e)
+          })
+      } else {
+        resolve('exclude')
+      }
+    })
+  }
+  /**
+   * 读取文件内容并调用loader
+   * @param {object} stats 文件信息
+   * @param {function} loader 加载器
+   * @param {function} done 处理完毕回调
+   */
+  loaderHandler(stats, done) {
+    const option = this.option
+    const loader = option.loader
+    if (typeof loader === 'function') {
+      if (stats.isDir || !option.readFile) {
+        loader(stats, '', done)
+      } else {
+        fs.readFile(stats.path, 'utf8', (err, data) => {
+          if (err) {
+            done()
           } else {
-            done('dir')
+            loader(stats, data, done)
           }
         })
-      } else {
-        done('exclude')
       }
-    })
-  } else {
-    done('exclude')
-  }
-}
-
-/**
- * 读取文件内容并调用loader
- * @param {object} stats 文件信息
- * @param {function} loader 加载器
- * @param {function} done 处理完毕回调
- */
-function loaderHandler(stats, loader, done) {
-  fs.readFile(stats.path, 'utf8', function (err, data) {
-    if (data && loader instanceof Function) {
-      loader(stats, data, done)
     } else {
       done()
     }
-  })
-}
-
-/**
- * 对文件夹进行验证
- * @param {string|RegExp} include 包含的字符
- * @param {string|RegExp} loader 排除的字符
- * @param {string} name 文件夹名
- */
-function verifyDir(include, exclude, name) {
-  let isBig = /^(node_modules)$/.test(name)
-  let isInclude = include ? include.test(name) : true
-  let isExclude = exclude ? exclude.test(name) : false
-
-  // 如果是比较大的文件夹, 必须在 include 的正则表达式里指定, 否则忽略
-  return isBig ? include && isInclude : isExclude
-}
-
-/**
- * 对文件进行验证
- * @param {string} filepath 文件路径
- * @param {string|RegExp} ext 扩展名
- */
-function verifyExt(filepath, ext) {
-  let isInclude = false
-  let info = path.parse(filepath)
-  let iext = info.ext ? info.ext.slice(1) : ''
-  if (ext && iext) {
-    if (typeof ext === 'string' && ext === iext) {
-      isInclude = true
-    } else if (ext instanceof RegExp && ext.test(iext)) {
-      isInclude = true
-    }
   }
-  return isInclude
-}
-
-module.exports = function exec(option) {
-  option = option || {}
-  if (!option.path) {
-    option.done && option.done()
-    return 
+  /**
+   * 对文件夹进行验证
+   * @param {string|RegExp} include 包含的字符
+   * @param {string|RegExp} loader 排除的字符
+   * @param {string} name 文件夹名
+   */
+  verifyDir(include, exclude, name) {
+    let isBig = /^(node_modules)$/.test(name)
+    let isInclude = include ? include.test(name) : true
+    let isExclude = exclude ? exclude.test(name) : false
+    // 如果是 比较大的文件夹, 必须在 include 的正则表达式里指定, 否则忽略
+    // 如果不是, 那么判断是否符合 包含且不排除
+    return isBig ? include && isInclude : isInclude && !isExclude
   }
-  search(option.path, function (stats, done) {
-    if (stats.isDir) {
-      done(verifyDir(option.include, option.exclude, stats.name))
-    } else {
-      if (verifyExt(stats.path, option.ext)) {
-        if (!option.loader) {
-          done(true)
-        } else if (option.readFile) {
-          loaderHandler(stats, option.loader, function () {
-            done(true)
-          })
-        } else {
-          option.loader(stats, '', function () {
-            done(true)
-          })
-        }
-      } else {
-        done(false)
+  /**
+   * 对文件进行验证
+   * @param {string} filepath 文件路径
+   * @param {string|RegExp} ext 扩展名
+   */
+  verifyExt(filepath, ext) {
+    let isInclude = false
+    let info = path.parse(filepath)
+    let iext = info.ext ? info.ext.slice(1) : ''
+    if (ext && iext) {
+      if (typeof ext === 'string' && ext === iext) {
+        isInclude = true
+      } else if (ext instanceof RegExp && ext.test(iext)) {
+        isInclude = true
       }
     }
-  }, function () {
-    option.done && option.done()
-  },
-  option)
+    return isInclude
+  }
+}
+
+module.exports = (option = {}) => {
+  option.path = option.path || './'
+  let loader = new Loader(Object.assign({}, option))
+  loader.search(loader.option.path).then(() => {
+    typeof loader.option.done === 'function' && loader.option.done()
+  })
 }
